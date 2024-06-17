0fb3d1e2d3cb1ad8456f9c76b4f0c831b172cb5e
Benjamin Jørgensen <me@benmi.me>
2024-06-17 22:55:32 +0900

    Can now login in to the project-a server

--- src/components/LoginItem.vue
+++ src/components/LoginItem.vue
@@ -2,16 +2,21 @@
 import { ref } from "vue";
 import { doLogin } from "../services/LoginService"
 
-const password = ref("")
-const username = ref("")
+
+
+const usernameInput = ref("")
+const passwordInput = ref("")
+
 
 function validateCredentials() {
   return true
 }
 
 async function loginUser() {
+  const username = usernameInput.value;
+  const password = passwordInput.value;
   if (validateCredentials()) {
-    doLogin(username.value, password.value)
+    await doLogin(username, password)
   }
 }
 
@@ -20,12 +25,12 @@ async function loginUser() {
 <template>
   <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
     <div class="mb-4">
-      <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="username" @click="loginUser">
         Username
       </label>
       <input
         class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-        id="username" type="text" placeholder="Username" :v-model="username.value" />
+        type="text" placeholder="Username" v-model="usernameInput" />
     </div>
     <div class="mb-6">
       <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
@@ -33,13 +38,13 @@ async function loginUser() {
       </label>
       <input
         class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-        id="password" type="password" placeholder="******************" :v-model="password.value" />
-      <p class="text-red-500 text-xs italic">Please choose a password.</p>
+        type="password" placeholder="******************" v-model="passwordInput" />
+      <p class="text-red-500 text-xs italic">Autentication failed. Please Check your credentials, then try again.</p>
     </div>
     <div class="flex items-center justify-between">
       <button
         class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-        type="submit" @click="loginUser">
+        @click="loginUser">
         Sign In
       </button>
       <a class=" inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
--- src/services/LoginService.ts
+++ src/services/LoginService.ts
@@ -1,33 +1,57 @@
+import { ref } from "vue";
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: 'github.com/benmi3',
+  baseURL: 'http://localhost:8080',
+  timeout: 1000,
   withCredentials: false,
   headers: {
     Accept: 'application/json',
-    'Content-Type': 'application',
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*'
+
   },
 })
 
-let successfull_login = false;
+const successfull_login = ref(false);
+const STORAGE_KEY_LOGIN_STATE = "LOGINSTATEKEY"
+
+async function requestLogin(username: string, password: string) {
+  apiClient.post('/api/login', {
+    "username": username,
+    "pwd": password
+  }).then(function(response) {
+    console.log(response);
+    setLoginState(response.data.result.success)
+  })
+    .catch(function(error) {
+      console.log(error);
+      setLoginState(false)
+    });
+
+
+}
+
+function setLoginState(value: boolean) {
+  successfull_login.value = value
+  console.log("I SET THE LOGIN TO", successfull_login.value)
+  localStorage.setItem(STORAGE_KEY_LOGIN_STATE, JSON.stringify(successfull_login.value))
+}
+
+function getStorageLoginState() {
+  const state = localStorage.getItem(STORAGE_KEY_LOGIN_STATE || '[]');
+  console.log("I GOT THE LOGIN STATE, ITS", state)
+  if (state) {
+    if (state == 'true') {
+      return true
+    }
+  }
+  return false
+}
 
 export async function doLogin(username: string, password: string) {
-  const login_response = await apiClient.post('/api/login', {
-    "username": username,
-    "password": password
-  })
-  try {
-    if (login_response.data.result.success == true) {
-      successfull_login = true
-    } else {
-      successfull_login = false
-
-    }
-  } catch {
-    successfull_login = false
-  }
-
-  return successfull_login
+  await requestLogin(username, password)
+  console.log("LOGIN REQUEST")
 }
 
 export async function doLogout() {
@@ -38,19 +62,22 @@ export async function doLogout() {
   })
   try {
     if (logout_response.data.result.logged_off == true) {
-      successfull_login = false
+      successfull_login.value = false
     } else {
-      successfull_login = false
+      successfull_login.value = false
 
     }
   } catch {
-    successfull_login = false
+    successfull_login.value = false
   }
   return successfull_login
 }
 
 export function isAuthenticated() {
+  successfull_login.value = getStorageLoginState()
+
   // will add a way to get this from
   // local storage aswell
-  return successfull_login
+  console.log("login state", successfull_login.value)
+  return successfull_login.value
 }
