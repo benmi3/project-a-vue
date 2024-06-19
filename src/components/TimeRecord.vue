e0f2b5bcd4169df027cf392693723a492b1c57e4
Benjamin Jørgensen <me@benmi.me>
2024-06-20 00:33:10 +0900

    added time input and place selcted now points to index 0 at first value.

--- src/components/TimeRecord.vue
+++ src/components/TimeRecord.vue
@@ -1,9 +1,11 @@
-<script setup>
+<script setup lang="ts">
 import { ref, onMounted } from 'vue'
 import { mainTimeRecordStore } from "../stores/timerecord.ts"
 const store = mainTimeRecordStore()
 const timerecords = ref(null)
+const starttime = ref(new Date())
 
+const stoptime = ref(new Date())
 
 onMounted(() => {
   timerecords.value = store.getTimeRecords()
@@ -23,12 +25,24 @@ const props = defineProps({
     required: true,
   },
 })
+
+const selectedPlace = ref(0)
+const optionPlace = ref(["Arendal", "Jobb"])
 </script>
 
 <template>
   <div>
     <h1>Potato</h1>
     <h2>{{ props.year }}/{{ props.month }}/{{ props.day }}</h2>
+    <input :v-model="props.year" />
+    <input :v-model="props.month" />
+    <input :v-model="props.day" />
+    <input type=time :v-model="starttime" />
+    <input type=time :v-model="stoptime" />
+    <select :selected="selectedPlace">
+      <option v-for="place in optionPlace" :key="place"> {{ place }}
+      </option>
+    </select>
   </div>
 </template>
 
